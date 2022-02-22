2bd6e7e088b14d7f6603ed41750962c8cf004b7b
shraddha sharma <sradha395@gmail.com>
2022-02-22 11:13:23 -0600

    added back button, ratings few css

--- movie.js
+++ movie.js
@@ -1,43 +1,76 @@
 var videos={
   movie1: {
-    name: "Movie 1",
-    url: "http://www.google.com",
+    name: "COCO",
+    image:"coco.jpg",
     title: "title",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa, cumque dolorem"
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa, cumque dolorem",
+    rating: "Rating "+[4.5]
+
   },
   movie2: {
-    name: "Movie 2",
-    url: "http://www.google.com",
+    name: "TANGLED",
+    image:"tangled.jpg",
     title: "title",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa, cumque dolorem"
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa, cumque dolorem",
+    rating:"Rating "+[4.2]
   },
   movie3: {
-    name: "Movie 3",
-    url: "#detail.html",
+    name: "FROZEN",
+    image:"frozen.jpg",
     title: "title",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa, cumque dolorem"
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa, cumque dolorem",
+    rating:"Rating "+[4.8]
+
   }
 }
 
 $( document ).ready(function () {
-  // console.log("dfsdfdsfsd");
-  var html = "";
+  
+ var html = "";
 
-  $.each(videos, function (key, value) {
+  $.each(videos, function (_key, value) {
     html += "<div id='movie'>"
-    html += "<div class=\"movie-image\"> <img src=\"tile.png\" width=\"300px\"> </div>";
+    html += "<div class=\"movie-image\"> <img src=\""+value.image+"\"> </div>";
     html += "<div class=\"movie-title\"> <h2><button onclick='goToDetailspage(value)'>"+value.name+"</button></h2> </div>";
     html += "<div class=\"movie-desc\"> <p>"+value.desc+"</p> </div>";
+    html += "<div class=\"rating\"> "+value.rating+"</div>"
     html += "</div>"
+  
   })
   document.getElementById("movies").innerHTML = html;
 
 })
 
 function goToDetailspage(videos) {
-  console.log("dfsdfdsfsd"+ videos.title);
+  window.location.href="newpage.html";
+  console.log(videos.name+""+videos.image); 
+
+
+  // console.log(goToDetailspage);
+
+
+
+}
+
+
+// $( document ).ready(function () {
+//   // console.log("dfsdfdsfsd");
+//   var newpage = "";
+
+// //  $.each(videos, function (_key, value) {
+//     newpage += "<div id='moviedetails'>"
+//     newpage += "<div class=\"movie-title\"> <h2><button onclick='goToDetailspage(value)'>"+value.name+"</button></h2> </div>";
+//     newpage += "<div class=\"movie-image\"> <img src=\""+value.image+"\"> </div>";
+//     newpage += "<div class=\"movie-desc\"> <p>"+value.desc+"</p> </div>";
+//     newpage += "</div>"
+  
+//   })
+//   document.getElementById("moviedetails").innerHTML = newpage;
+
+// // })-->
+
+
   // navigate to new page
   // render html
 
-}
 
